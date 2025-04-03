5bb99d1a732c0c95c6244f3b82d1daca18f5541c
brown85858585 <151971750+brown85858585@users.noreply.github.com>
2025-04-03 16:04:37 +0300

    Leaderboard

--- Assets/Packeges/LeaderboardCreator/Scripts/Main/Leaderboards.cs
+++ Assets/Packeges/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -4,5 +4,6 @@ namespace Dan.Main
     {
         public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
         public static LeaderboardReference TestLudumeLB = new LeaderboardReference("69b81867b2bb7280be917d1dcaabf3464e63c3016a2f715685d55a4e018d0e61");
+        public static LeaderboardReference TestLDboard = new LeaderboardReference("69b81867b2bb7280be917d1dcaabf3464e63c3016a2f715685d55a4e018d0e61");
     }
 }
