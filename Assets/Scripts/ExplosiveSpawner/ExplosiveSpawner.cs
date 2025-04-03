2ff2ca32df417e9611d13ad41196f67b7841ab0f
Sergey <kondor07@mail.ru>
2025-04-03 18:51:55 +0300

    Working with a Explosive Spawner

--- Assets/Scripts/ExplosiveSpawner/ExplosiveSpawner.cs
+++ Assets/Scripts/ExplosiveSpawner/ExplosiveSpawner.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Supporting;
 
@@ -10,11 +8,14 @@ public class ExplosiveSpawner : MonoBehaviour
     [SerializeField] private float _delay = 1.0f;
     [SerializeField] private float _repeatRate = 3.0f;
 
+    [SerializeField] private GameObject _prefabDangerZone;
     [SerializeField] private GameObject _prefabBall;
 
     private ObjectPool _objectPool;
     private void Start()
     {
+        _prefabDangerZone.transform.localScale = new Vector3(_distance * 2.0f, 0.1f, _distance * 2.0f);
+        //_prefabDangerZone.transform.localScale.y = 0.1f;
         _objectPool = new ObjectPool(_ballAmount, _prefabBall);
         InvokeRepeating("SpawnBall", _delay, _repeatRate);
     }
@@ -28,6 +29,7 @@ public class ExplosiveSpawner : MonoBehaviour
             float randomZ = Random.Range(transform.position.z - _distance, transform.position.z + _distance);
             ball.transform.position = new Vector3(randomX, 0, randomZ);
             ball.GetComponent<ExplosiveBall>().Reset();
+            //ball.GetComponentInChildren<LightningEffect>().SetRadius(ball.transform.localScale.x * 0.9f);
             ball.SetActive(true);
         }
     }
