b3631893dc0a5663da9e6181896ff88fc4d74b27
Sergey <kondor07@mail.ru>
2025-04-03 13:29:17 +0300

    Working with the player and the camera

--- Assets/Scripts/ExplosiveSpawner/ExplosiveSpawner.cs
+++ Assets/Scripts/ExplosiveSpawner/ExplosiveSpawner.cs
@@ -5,7 +5,7 @@ using Supporting;
 
 public class ExplosiveSpawner : MonoBehaviour
 {
-    [SerializeField] private float _radius = 10.0f;
+    [SerializeField] private float _distance = 10.0f;
     [SerializeField] private int _ballAmount = 10;
     [SerializeField] private float _delay = 1.0f;
     [SerializeField] private float _repeatRate = 3.0f;
@@ -24,8 +24,8 @@ public class ExplosiveSpawner : MonoBehaviour
 
         if (ball != null)
         {
-            float randomX = Random.Range(transform.position.x - _radius, transform.position.x + _radius);
-            float randomZ = Random.Range(transform.position.z - _radius, transform.position.z + _radius);
+            float randomX = Random.Range(transform.position.x - _distance, transform.position.x + _distance);
+            float randomZ = Random.Range(transform.position.z - _distance, transform.position.z + _distance);
             ball.transform.position = new Vector3(randomX, 0, randomZ);
             ball.GetComponent<ExplosiveBall>().Reset();
             ball.SetActive(true);
